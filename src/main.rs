7eff5d8dbbf053ba74b723c2fe457c9bbd96333c
Brendon-Hablutzel <booklvrbren@gmail.com>
2023-10-07 17:47:42 -0400

    add trig functions, natural logarithm, pi, and e

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::{E, PI};
+
 #[derive(Debug, PartialEq)]
 enum Token {
     Number(u32),
@@ -9,6 +11,12 @@ enum Token {
     Carat,
     OpenParen,
     CloseParen,
+    Sin,
+    Cos,
+    Tan,
+    Ln,
+    Pi,
+    E,
     Eof,
 }
 
@@ -54,7 +62,16 @@ impl<'a> Lexer<'a> {
         }
 
         let ident: String = ident_chars.iter().collect();
-        (Token::Ident(ident), ident_chars.len())
+
+        match ident.as_str() {
+            "sin" => (Token::Sin, 3),
+            "cos" => (Token::Cos, 3),
+            "tan" => (Token::Tan, 3),
+            "ln" => (Token::Ln, 2),
+            "pi" => (Token::Pi, 2),
+            "e" => (Token::E, 1),
+            _ => (Token::Ident(ident), ident_chars.len()),
+        }
     }
 
     fn tokenize_number(&mut self) -> (Token, usize) {
@@ -184,7 +201,7 @@ impl<'a> Parser<'a> {
     }
 
     fn factor(&mut self) -> f32 {
-        let mut left = self.base();
+        let mut left = self.primary();
 
         while let Some(next_token) = self.peek_next_token() {
             match next_token {
@@ -199,21 +216,10 @@ impl<'a> Parser<'a> {
         left
     }
 
-    fn base(&mut self) -> f32 {
-        let mut result = 0.0;
-
-        while let Some(next_token) = self.peek_next_token() {
-            match next_token {
-                Token::Number(number) => {
-                    result = *number as f32;
-                    self.consume_next_token(Token::Number(*number));
-                }
-                Token::OpenParen => result = self.parenthesized(),
-                Token::Minus => result = self.unary(),
-                _ => break,
-            }
-        }
-        result
+    fn unary(&mut self) -> f32 {
+        self.consume_next_token(Token::Minus);
+        let result = self.factor();
+        -1.0 * result
     }
 
     fn parenthesized(&mut self) -> f32 {
@@ -223,23 +229,63 @@ impl<'a> Parser<'a> {
         result
     }
 
-    fn unary(&mut self) -> f32 {
-        self.consume_next_token(Token::Minus);
-        let result = self.factor();
-        -1.0 * result
+    fn primary(&mut self) -> f32 {
+        if let Some(next_token) = self.peek_next_token() {
+            match next_token {
+                Token::Number(number) => {
+                    let result = *number as f32;
+                    self.consume_next_token(Token::Number(*number));
+                    result
+                }
+                Token::OpenParen => self.parenthesized(),
+                Token::Minus => self.unary(),
+                Token::Pi => {
+                    self.consume_next_token(Token::Pi);
+                    PI
+                }
+                Token::E => {
+                    self.consume_next_token(Token::E);
+                    E
+                }
+                Token::Sin => {
+                    self.consume_next_token(Token::Sin);
+                    let arg_value = self.parenthesized();
+                    f32::sin(arg_value)
+                }
+                Token::Cos => {
+                    self.consume_next_token(Token::Cos);
+                    let arg_value = self.parenthesized();
+                    f32::cos(arg_value)
+                }
+                Token::Tan => {
+                    self.consume_next_token(Token::Tan);
+                    let arg_value = self.parenthesized();
+                    f32::tan(arg_value)
+                }
+                Token::Ln => {
+                    self.consume_next_token(Token::Ln);
+                    let arg_value = self.parenthesized();
+                    f32::ln(arg_value)
+                }
+                _ => panic!("Unrecognized token: {:?}", next_token),
+            }
+        } else {
+            0.0
+        }
     }
 }
 
 // GRAMMAR:
-// expression => term + term | term - term | term
-// term => factor * factor | factor / factor | factor
-// factor => base ^ factor | base
-// base => NUMBER | unary | parenthesized
-// parenthesized => ( expression )
-// unary => - factor
+// expression       => term + term | term - term | term
+// term             => factor * factor | factor / factor | factor
+// factor           => primary ^ factor | primary
+// unary            => - factor
+// function         => "sin" | "cos" | "tan" | "ln"
+// parenthesized    => ( expression )
+// primary          => NUMBER | unary | parenthesized | function parenthesized | "pi" | "e"
 
 fn main() {
-    let s = "7*(2+4/2)+(-2)^-2";
+    let s = "ln(e)*sin(pi/4)";
     println!("{s}");
 
     let mut lexer = Lexer::new(s);
